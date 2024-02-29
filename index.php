ccea0c10d5778e561e4de0f8d53bef735771998a
Дмитрий Гайдабура <reblack@bk.ru>
2024-02-29 11:29:04 +0300

    Update index.php
    Example  calls extended

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 //autoload...
 
-$service = Factory\ServiceFactory::build(Factory\ServiceFactory::EMAIL);
-$service->send('xxxx', 'sadsad');
+$serviceEmail = Factory\ServiceFactory::build(Factory\ServiceFactory::EMAIL);
+$serviceEmail->send('xxxx@wwww.aa', 'sadsad');
+
+$serviceSms = Factory\ServiceFactory::build(Factory\ServiceFactory::SMS);
+$serviceSms->send('78721321322', 'sadsad');
